0af7de59e79cbc92cd47cf0ba713499c3844d80f
tejasg14aws2-alt <tejasg14aws2@gmail.com>
2025-12-30 18:25:51 +0530

    Update index.php
    change the text

--- myphp/index.php
+++ myphp/index.php
@@ -2,7 +2,7 @@
 
 <div class="content">
     <h2>Welcome 👋</h2>
-    <p>Welcome to Linux Learning</p>
+    <p>Welcome to Linux Learning 2026</p>
     <p>Server Time: <?php echo date("d-m-Y H:i:s"); ?></p>
 </div>
 
